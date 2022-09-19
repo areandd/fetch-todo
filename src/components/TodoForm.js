fdd1a7bdb778a05c36de430c3af166264583e98b
Isaiah Desta <areandd@protonmail.com>
2022-09-19 17:14:11 -0400

    final touches

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -25,17 +25,7 @@ function TodoForm(props) {
     <form onSubmit={handleSubmit} className='todo-form'>
       {props.edit ? (
         <>
-          <input
-            placeholder='Update your item'
-            value={input}
-            onChange={handleChange}
-            name='text'
-            ref={inputRef}
-            className='todo-input edit'
-          />
-          <button onClick={handleSubmit} className='todo-button edit'>
-            Update
-          </button>
+         
         </>
       ) : (
         <>
