48576ba0e916e4ddfb7802f8e6fb165f034a4267
Isaiah Desta <areandd@protonmail.com>
2022-09-26 15:03:08 -0400

    api init

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
 
+
 function TodoForm(props) {
+  const url = "https://assets.breatheco.de/apis/fake/todos/user/areandd";
   const [input, setInput] = useState("");
-  const inputRef = useRef(null);
+  const inputRef = useRef();
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
+
+  const addTodo = todo => {
+    if (todo === '') {
+      return;
+    }
+    // setTodos([...todos, {label:todo, done:false}]);
+    updateTodos([...props.todos, {label:todo, done:false}]);
+  };
+
+  const updateTodos = (data) => {
+    fetch(url, {
+    method: 'PUT', // or 'POST'
+    body: JSON.stringify(data), // data can be a `string` or  an {object} which comes from somewhere further above in our application
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+      if (!res.ok) throw Error(res.statusText);
+      return res.json();
+  })
+  .then(response => props.func(response))
+  .catch(error => console.error(error));
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,13 +6,7 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
   const url = "https://assets.breatheco.de/apis/fake/todos/user/areandd";
   
-  const addTodo = todo => {
-    if (!todo.text || /^\s*$/.test(todo.text)) {
-      return;
-    }
-    setTodos([todo, ...todos]);
-    console.log(...todos);
-  };
+
   const removeTodo = id => {
     const removedArr = [...todos].filter(todo => todo.id !== id);
     setTodos(removedArr);
@@ -32,10 +26,14 @@ function TodoList() {
   }, []);
 
 
+
+
+  console.log(todos);
+
   return (
     <>
       <h1>todo list</h1>
-      <TodoForm onSubmit={addTodo} />
+      <TodoForm todos={todos} func={setTodos} />
       <Todo
         todos={todos}
         removeTodo={removeTodo}
